ad9293d1ff1593d68884d0f1cbdff421b8b85438
mishraprince0786@gmail.com <mishraprince0786@gmail.com>
2018-12-24 16:41:40 +0530

    ready to deploy

--- config/config_dev.js
+++ config/config_dev.js
@@ -2,10 +2,7 @@
 
 module.exports = {
     PORT : 3000,
-
-    //change with MLAB url
     DB_URL : 'mongodb://localhost:27017/scalelabs',
-
     SECRET_KEY : 'qwerty',
     TIMEOUT : 1*60*60  ,     //1 hr Expiry Duration
     CLOUD_NAME : 'dt7slkdyy',
